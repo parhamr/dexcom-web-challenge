25e2195984a63921e7a9cf65552a52b60475907e
parhamr <reid.parham@dexcom.com>
2017-10-15 23:05:40 -0700

    Keep it sorted for convenient comparison

--- theirs/incremental_interquartile_mean.rb
+++ theirs/incremental_interquartile_mean.rb
@@ -5,7 +5,7 @@ File.open('data.txt','r') do |f|
     data << l.to_i
     if data.length >= 4
       q = data.size / 4.0
-      ys = data.sort[q.ceil-1..(3*q).floor]
+      ys = data.sort![q.ceil-1..(3*q).floor]
       factor = q - (ys.size/2.0 - 1)
 
       mean = (ys[1...-1].inject(0, :+) + (ys[0] + ys[-1]) * factor) / (2*q)
